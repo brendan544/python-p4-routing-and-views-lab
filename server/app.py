f539f06fb4c31b6c9f6ef01c8d04a050665d46d7
brendan544 <brendan.gwer@student.moringaschool.com>
2024-10-03 10:16:53 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,8 +1,74 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, jsonify
+import sys
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>', 200
+
+@app.route('/print/<parameter>')
+def print_parameter(parameter):
+    print(parameter)  # This will output to console
+    return parameter, 200
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    return '\n'.join(str(i) for i in range(parameter)) + '\n', 200
+
+@app.route('/math/<int:left>/<operation>/<int:right>')
+def math(left, operation, right):
+    if operation == '+':
+        result = left + right
+    elif operation == '-':
+        result = left - right
+    elif operation == '*':
+        result = left * right
+    elif operation == 'div':
+        result = left / right
+    elif operation == '%':
+        result = left % right
+    else:
+        return "Invalid operation", 400
+    
+    return str(result), 200
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
